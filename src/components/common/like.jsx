6d302feee33049db5a2122f75d572a7f0665d3bf
mikemorter <mikemorter@gmail.com>
2022-03-09 11:21:11 -0500

    added pagination and installed lodash

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
-const Like = () => {
+const Like = (props) => {
   let classes = "fa fa-heart";
-  if (!this.props.liked) classes += "-o";
+  if (!props.liked) classes += "-o";
   return (
     <i
-      onClick={this.props.onClick}
+      onClick={props.onClick}
       style={{ cursor: "pointer" }}
       className={classes}
       aria-hidden="true"
