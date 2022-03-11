84f1c479f248f7a49c6aa5735597a4894ac69881
mikemorter <mikemorter@gmail.com>
2022-03-11 10:29:33 -0500

    Added login register navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
+import NotFound from "./components/notFound";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <div>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies/new" component={MovieForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/movies" component={Movies} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </div>
   );
 }
 
